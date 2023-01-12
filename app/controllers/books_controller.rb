bb7e8a88e4414da9ba12530bbd4fd20147f37308
Mouthvalley <yamatotaniguchi1107@gmail.com>
2023-01-12 09:06:17 +0000

    [Add]showto

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.new
+    @book = Book.new
     # List.allはテーブルに保存されたデータを全取得する
     @books = Book.all
   end
@@ -11,15 +11,22 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/books'
+    # 詳細画面へリダイレクト
+    redirect_to book_path(book.id)
   end
 
   def show
-    @books = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
    private
