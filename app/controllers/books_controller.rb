3a595f0464197a1f2f26f4003f5b9f11b8b8786c
Mouthvalley <yamatotaniguchi1107@gmail.com>
2023-01-13 02:14:27 +0000

    [Add]Delete機能/バリデーションによるエラーメッセージを表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,12 +7,12 @@ class BooksController < ApplicationController
   end
 
   def create
-     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 詳細画面へリダイレクト
-    redirect_to book_path(book.id)
+     @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def show
@@ -23,10 +23,16 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-   def update
+  def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
 
    private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/homes' => 'homes#top'
 
-  get '/books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # コントローラの同じ名前のアクションに紐づく形で、以下のルーティングを自動生成する
+  resources :books
 end
